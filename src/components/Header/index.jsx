dc0b7a81af2cc30234bd8726ec000e58feca9552
Sofia boud <sofiasoul@hotmail.fr>
2021-06-28 14:39:42 +0200

    header ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,11 @@
+import SBarNav from './style';
+
 function Header() {
-  return <p>fiverr.</p>;
+  return (
+    <SBarNav>
+      <img src="/image/Frame_1.png" alt="logo" />
+      <div className="header" />
+    </SBarNav>
+  );
 }
 export default Header;
